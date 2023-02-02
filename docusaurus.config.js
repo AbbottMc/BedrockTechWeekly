f5ca42eb2ab816b67f803284960d1c9281a91076
QianShanyao <qian_shan_yao@163.com>
2023-02-03 04:12:51 +0800

    通过travis集成部署至github page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'AbbottMc', // Usually your GitHub org/user name.
-  projectName: 'BedrockTech', // Usually your repo name.
+  projectName: 'BedrockTechWeekly', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
