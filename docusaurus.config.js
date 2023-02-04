39a7fb219e9d7b6dc3f86fbd042132fc8c82fe16
QianShanyao <qian_shan_yao@163.com>
2023-02-04 18:53:14 +0800

    更新站点本地化，更改编辑页面链接

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,8 +28,8 @@ const config = {
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'zh',
+    locales: ['zh'],
   },
 
   presets: [
@@ -42,7 +42,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/AbbottMc/BedrockTechWeekly/tree/master',
         },
         blog: {
           blogSidebarTitle: '历史周刊文章',
@@ -51,7 +51,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/AbbottMc/BedrockTechWeekly/tree/master',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
